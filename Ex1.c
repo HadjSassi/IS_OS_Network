4267033e2474e8bc5eea6a028ebaa924326e10e8
Chahrazed baatout <baatoutchahrazed@gmail.com>
2025-09-23 08:33:09 +0100

    refactoring ex1

--- Ex1.c
+++ Ex1.c
@@ -1,24 +1,33 @@
+#define GLOBAL_INIT   42
+#define GLOBAL_ZERO   0
+#define STRING_LENGTH    16
+
+#define GLOBAL_STRING "Hello World"
+#define PMAP_PATH      "pmap"
+#define PMAP_BIN      "pmap"
+#define PMAP_OPTION   "-X"
+#define DATA_TEXT "Data  :"
+#define BSS_TEXT "BSS  :"
+#define STR_TEXT "STR  :"
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-int global_init = 42;
-
-int global_zero = 0;
-
-char *str = "Hello World";
+int global_init = GLOBAL_INIT;
+int global_zero = GLOBAL_ZERO;
+char *global_string = GLOBAL_STRING;
 
 int main(void) {
+    printf("%s %p\n",DATA_TEXT, (void *)&global_init);
+    printf("%s %p\n",BSS_TEXT, (void *)&global_zero);
+    printf("%s %p\n",STR_TEXT, (void *)global_string);
 
+    char command[STRING_LENGTH];
+    snprintf(command, sizeof(command), "%d", getpid());
 
-    printf("Data : %p\n", (void*)&global_init);
-    printf("BSS  : %p\n", (void*)&global_zero);
-    printf("Str  : %p\n", (void*)str);
-
-    printf("\n Hello memory  :\n");
-    char cmd[64];
-    snprintf(cmd, sizeof(cmd), "pmap -X %d", getpid());
-    system(cmd);
+    execlp(PMAP_PATH, PMAP_BIN, PMAP_OPTION, command, (char *)NULL);
 
     return 0;
 }
