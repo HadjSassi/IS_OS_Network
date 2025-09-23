718e5cde88558496881a2953df1aa689b99da764
HADJ SASSI Mahdi <mahdihadjsassi1@gmail.com>
2025-09-23 10:11:24 +0200

    reformulate Ex2.c

--- Ex2.c
+++ Ex2.c
@@ -22,55 +22,77 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-int main() {
-    const char *filename = FILE_NAME;
-    const char *new_content = TEXT_CONTENT;
 
+void write_file(const char *filename, const char *content) {
     int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, OPEN_FLAG);
     if (fd < FAILURE_FLAG) {
         perror(OPENING_ERROR);
-        return FAILURE_EXIT;
+        exit(FAILURE_EXIT);
     }
-    ssize_t written = write(fd, new_content, strlen(new_content));
+    ssize_t written = write(fd, content, strlen(content));
     if (written < FAILURE_FLAG) {
         perror(WRITING_ERROR);
         close(fd);
-        return FAILURE_EXIT;
+        exit(FAILURE_EXIT);
     }
     close(fd);
+}
 
-    fd = open(filename, O_RDWR);
+int open_file_rw(const char *filename) {
+    int fd = open(filename, O_RDWR);
     if (fd < FAILURE_FLAG) {
         perror(OPENING_ERROR);
-        return FAILURE_EXIT;
+        exit(FAILURE_EXIT);
     }
+    return fd;
+}
 
+void get_file_size(int fd, size_t *size) {
     struct stat st;
     if (fstat(fd, &st) < FAILURE_FLAG) {
         perror(FILE_STATUS_ERROR);
-        close(fd);
-        return FAILURE_EXIT;
+        exit(FAILURE_EXIT);
     }
-    size_t size = st.st_size;
-    printf("%s %zu %s\n",TEXT_FILE_SIZE, size,FILE_UNIT);
+    *size = st.st_size;
+}
 
+char* map_file(int fd, size_t size) {
     char *data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, OFFSET);
     if (data == MAP_FAILED) {
         perror(MMAP_ERROR);
-        close(fd);
-        return FAILURE_EXIT;
+        exit(FAILURE_EXIT);
     }
+    return data;
+}
 
-    printf("\n\n%s\n\n%.*s\n\n",TEXT_BEFORE_INSERTION, (int) size, data);
-
-
+void reverse_content(char *data, size_t size) {
     for (size_t i = OFFSET; i < size / 2; i++) {
         char tmp = data[i];
         data[i] = data[size - 1 - i];
         data[size - 1 - i] = tmp;
     }
+}
 
-    printf("\n\n%s\n\n%.*s\n\n",TEXT_AFTER_INSERTION, (int) size, data);
+int main() {
+    const char *filename = FILE_NAME;
+    const char *new_content = TEXT_CONTENT;
+    size_t size;
+
+    write_file(filename, new_content);
+
+    int fd = open_file_rw(filename);
+
+    get_file_size(fd, &size);
+
+    printf("%s %zu %s\n", TEXT_FILE_SIZE, size, FILE_UNIT);
+
+    char *data = map_file(fd, size);
+
+    printf("\n\n%s\n\n%.*s\n\n", TEXT_BEFORE_INSERTION, (int)size, data);
+
+    reverse_content(data, size);
+
+    printf("\n\n%s\n\n%.*s\n\n", TEXT_AFTER_INSERTION, (int)size, data);
 
     munmap(data, size);
     close(fd);
